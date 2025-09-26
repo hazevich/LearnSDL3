94abf1f1871b8083894730fa230bb328284889ea
tchanturia <t.d.chanturia@gmail.com>
2025-09-26 21:50:42 +0200

    hello world

---
+++ src/LearnSDL3.cpp
@@ -0,0 +1,12 @@
+﻿// LearnSDL3.cpp : Defines the entry point for the application.
+//
+
+#include "LearnSDL3.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
