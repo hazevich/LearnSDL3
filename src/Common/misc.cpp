cc232fba47a482b11371fa538ddadb4c43ec2310
tchanturia <t.d.chanturia@gmail.com>
2025-11-07 12:27:15 +0100

    shader, pipeline and transfer buffer abstractions

---
+++ src/Common/misc.cpp
@@ -0,0 +1,229 @@
+#include "misc.h"
+#include "SDL3/SDL.h"
+
+std::optional<Shader> Shader::FromSPV(SDL_GPUDevice* graphicsDevice, const std::string& shaderPath, const ShaderCreateInfo& shaderCreateInfo)
+{
+    size_t shaderCodeSize;
+    void* shaderCode = SDL_LoadFile(shaderPath.c_str(), &shaderCodeSize);
+
+    if (shaderCode == NULL)
+    {
+        SDL_Log("Failed to load shader");
+        return std::nullopt;
+    }
+
+    SDL_GPUShaderCreateInfo shaderInfo = {
+        .code_size = shaderCodeSize,
+        .code = (uint8_t*)shaderCode,
+        .entrypoint = "main",
+        .format = SDL_GPU_SHADERFORMAT_SPIRV,
+        .stage = shaderCreateInfo.ShaderStage,
+        .num_samplers = shaderCreateInfo.NumSamplers,
+        .num_storage_textures = shaderCreateInfo.NumStorageTextures,
+        .num_storage_buffers = shaderCreateInfo.NumStorageBuffers,
+        .num_uniform_buffers = shaderCreateInfo.NumUniformBuffers,
+    };
+
+    SDL_GPUShader* vertexShader = SDL_CreateGPUShader(graphicsDevice, &shaderInfo);
+
+    if (vertexShader == NULL)
+    {
+        SDL_Log("Failed to create shader: %s", SDL_GetError());
+        return std::nullopt;
+    }
+
+    SDL_free(shaderCode);
+
+    Shader shader(graphicsDevice, vertexShader);
+
+    return shader;
+}
+
+Shader::Shader(SDL_GPUDevice* graphicsDeviceHandle, SDL_GPUShader* shaderHandle)
+    : GraphicsDeviceHandle(graphicsDeviceHandle), ShaderHandle(shaderHandle)
+{ }
+
+SDL_GPUShader* Shader::GetHandle() const
+{
+    return ShaderHandle;
+}
+
+void Shader::Release()
+{
+    SDL_ReleaseGPUShader(GraphicsDeviceHandle, ShaderHandle);
+}
+
+Pipeline::Pipeline(SDL_GPUDevice* graphicsDevice, SDL_GPUGraphicsPipeline* pipelineHandle)
+    : GraphicsDeviceHandle(graphicsDevice), PipelineHandle(pipelineHandle)
+{
+}
+
+void Pipeline::Release()
+{
+    SDL_ReleaseGPUGraphicsPipeline(GraphicsDeviceHandle, PipelineHandle);
+}
+
+SDL_GPUGraphicsPipeline* Pipeline::GetHandle() const
+{
+    return PipelineHandle;
+}
+
+std::optional<Pipeline> Pipeline::Create(SDL_GPUDevice* graphicsDevice, SDL_Window* window, PipelineCreateInfo createInfo)
+{
+    VertexBufferDescription& vertexBufferDescription = createInfo.VertexBufferDescription;
+
+    SDL_GPUVertexBufferDescription vertexBufferDescriptions[] = {
+        {
+            .slot = vertexBufferDescription.Slot,
+            .pitch = vertexBufferDescription.Pitch,
+            .input_rate = SDL_GPU_VERTEXINPUTRATE_VERTEX,
+            .instance_step_rate = 0
+        }
+    };
+
+    SDL_GPUColorTargetDescription colorTargetDescriptions[1];
+    colorTargetDescriptions[0] = {
+        .format = SDL_GetGPUSwapchainTextureFormat(graphicsDevice, window)
+    };
+        
+
+    std::vector<SDL_GPUVertexAttribute>& vertexAttributes = createInfo.VertexAttributes;
+
+    SDL_GPUGraphicsPipelineCreateInfo pipelineInfo = {
+        .vertex_shader = createInfo.VertexShader->GetHandle(),
+        .fragment_shader = createInfo.FragmentShader->GetHandle(),
+        .vertex_input_state = {
+            .vertex_buffer_descriptions = vertexBufferDescriptions,
+            .num_vertex_buffers = 1,
+            .vertex_attributes = vertexAttributes.data(),
+            .num_vertex_attributes = (uint32_t) vertexAttributes.size()
+        },
+        .primitive_type = SDL_GPU_PRIMITIVETYPE_TRIANGLELIST,
+        .target_info = {
+            .color_target_descriptions = colorTargetDescriptions,
+            .num_color_targets = 1,
+        }
+    };
+
+    SDL_GPUGraphicsPipeline* pipelineHandle = SDL_CreateGPUGraphicsPipeline(graphicsDevice, &pipelineInfo);
+
+    if (pipelineHandle == NULL)
+    {
+        SDL_Log("Failed to create graphics pipeline: %s", SDL_GetError());
+        return std::nullopt;
+    }
+
+    return Pipeline(graphicsDevice, pipelineHandle);
+}
+
+GPUUploader::GPUUploader(SDL_GPUDevice* graphicsDevice)
+    : _graphicsDevice(graphicsDevice)
+{
+
+}
+
+void GPUUploader::AddVertexData(float vertices[], uint32_t size, SDL_GPUBuffer* vertexBuffer, uint32_t bufferOffset)
+{
+    Vertices.push_back(VertexData{ .Vertices = vertices, .Size = size, .VertexBuffer = vertexBuffer, .BufferOffset = bufferOffset });
+}
+
+void GPUUploader::AddIndexData(uint32_t indecies[], uint32_t size, SDL_GPUBuffer* indexBuffer, uint32_t bufferOffset)
+{
+    Indecies.push_back(IndexData{ .Indecies = indecies, .Size = size, .IndexBuffer = indexBuffer, .BufferOffset = bufferOffset });
+}
+
+bool GPUUploader::Upload()
+{
+    uint32_t totalSize = 0;
+
+    for (VertexData& vertexData : Vertices)
+    {
+        totalSize += vertexData.Size;
+    }
+
+    for (IndexData& indexData : Indecies)
+    {
+        totalSize += indexData.Size;
+    }
+
+    SDL_GPUTransferBufferCreateInfo createInfo = { .usage = SDL_GPU_TRANSFERBUFFERUSAGE_UPLOAD, .size = totalSize };
+    SDL_GPUTransferBuffer* transferBuffer = SDL_CreateGPUTransferBuffer(_graphicsDevice, &createInfo);
+
+    if (transferBuffer == NULL)
+    {
+        SDL_Log("Failed to create transfer buffer: %s", SDL_GetError());
+        return false;
+    }
+
+    void* transferData = SDL_MapGPUTransferBuffer(_graphicsDevice, transferBuffer, false);
+    uint32_t transferDataOffset = 0;
+
+    for (VertexData& vertexData : Vertices)
+    {
+        SDL_memcpy((float*) transferData + transferDataOffset, vertexData.Vertices, vertexData.Size);
+        transferDataOffset += vertexData.Size;
+    }
+
+    for (IndexData& indexData : Indecies)
+    {
+        SDL_memcpy((uint32_t*) transferData + transferDataOffset, indexData.Indecies, indexData.Size);
+        transferDataOffset += indexData.Size;
+    }
+
+    SDL_UnmapGPUTransferBuffer(_graphicsDevice, transferBuffer);
+
+    SDL_GPUCommandBuffer* commandBuffer = SDL_AcquireGPUCommandBuffer(_graphicsDevice);
+    if (commandBuffer == NULL)
+    {
+        SDL_Log("Failed to acquire a command buffer: %s", SDL_GetError());
+        return false;
+    }
+
+    SDL_GPUCopyPass* copyPass = SDL_BeginGPUCopyPass(commandBuffer);
+    transferDataOffset = 0;
+
+    for (VertexData& vertexData : Vertices)
+    {
+        SDL_GPUTransferBufferLocation transferBufferLocation = {
+            .transfer_buffer = transferBuffer,
+            .offset = transferDataOffset,
+        };
+
+        SDL_GPUBufferRegion bufferRegion = {
+            .buffer = vertexData.VertexBuffer,
+            .offset = vertexData.BufferOffset,
+            .size = vertexData.Size,
+        };
+
+        SDL_UploadToGPUBuffer(copyPass, &transferBufferLocation, &bufferRegion, false);
+
+        transferDataOffset += vertexData.Size;
+    }
+
+    for (IndexData& indexData : Indecies)
+    {
+        SDL_GPUTransferBufferLocation transferBufferLocation = {
+            .transfer_buffer = transferBuffer,
+            .offset = transferDataOffset,
+        };
+
+        SDL_GPUBufferRegion bufferRegion = {
+            .buffer = indexData.IndexBuffer,
+            .offset = indexData.BufferOffset,
+            .size = indexData.Size,
+        };
+
+        SDL_UploadToGPUBuffer(copyPass, &transferBufferLocation, &bufferRegion, false);
+
+        transferDataOffset += indexData.Size;
+    }
+
+    SDL_EndGPUCopyPass(copyPass);
+    SDL_SubmitGPUCommandBuffer(commandBuffer);
+    SDL_ReleaseGPUTransferBuffer(_graphicsDevice, transferBuffer);
+
+    Vertices.clear();
+    Indecies.clear();
+
+    return true;
+}
--- src/Common/misc.h
+++ src/Common/misc.h
@@ -1,16 +1,107 @@
 #pragma once
 
 #include <cstdint>
-#include "SDL3/SDL.h"
+#include <string>
+#include <optional>
+#include <vector>
+
+#include <SDL3/SDL.h>
 
 static const float NanosecondInSecond = 1e9f;
 
-static inline float NanosecondsToSeconds(float nanoseconds)
+inline float NanosecondsToSeconds(float nanoseconds)
 {
     return nanoseconds / NanosecondInSecond;
 }
 
-static inline float SDL_GetTimeSeconds()
+inline float SDL_GetTimeSeconds()
 {
     return NanosecondsToSeconds(SDL_GetTicksNS());
 }
+
+struct ShaderCreateInfo 
+{
+    SDL_GPUShaderStage ShaderStage;
+    uint32_t NumSamplers = 0;
+    uint32_t NumStorageTextures = 0;
+    uint32_t NumStorageBuffers = 0;
+    uint32_t NumUniformBuffers = 0;
+};
+
+class Shader
+{
+public:
+    static std::optional<Shader> FromSPV(SDL_GPUDevice* graphicsDevice, const std::string& shaderPath, const ShaderCreateInfo& shaderCreateInfo);
+
+    SDL_GPUShader* GetHandle() const;
+
+    void Release();
+
+private:
+    SDL_GPUDevice* GraphicsDeviceHandle;
+    SDL_GPUShader* ShaderHandle;
+
+    Shader(SDL_GPUDevice* graphicsDeviceHandle, SDL_GPUShader* shaderHandle);
+};
+
+struct VertexBufferDescription
+{
+    uint32_t Slot;
+    uint32_t Pitch;
+};
+
+struct PipelineCreateInfo
+{
+    Shader* VertexShader;
+    Shader* FragmentShader;
+    VertexBufferDescription VertexBufferDescription;
+    std::vector<SDL_GPUVertexAttribute> VertexAttributes;
+};
+
+class Pipeline
+{
+public:
+    static std::optional<Pipeline> Create(SDL_GPUDevice* graphicsDevice, SDL_Window* window, PipelineCreateInfo createinfo);
+
+    void Release();
+
+    SDL_GPUGraphicsPipeline* GetHandle() const;
+
+private:
+    SDL_GPUGraphicsPipeline* PipelineHandle;
+    SDL_GPUDevice* GraphicsDeviceHandle;
+
+    Pipeline(SDL_GPUDevice* graphicsDevice, SDL_GPUGraphicsPipeline* pipelineHandle);
+};
+
+class GPUUploader
+{
+public:
+    GPUUploader(SDL_GPUDevice* graphicsDevice);
+
+    void AddVertexData(float vertices[], uint32_t size, SDL_GPUBuffer* vertexBuffer, uint32_t bufferOffset = 0);
+    void AddIndexData(uint32_t indecies[], uint32_t size, SDL_GPUBuffer* indexBuffer, uint32_t bufferOffset = 0);
+    bool Upload();
+
+private:
+    struct VertexData
+    {
+        float* Vertices;
+        uint32_t Size;
+        SDL_GPUBuffer* VertexBuffer;
+        uint32_t BufferOffset;
+    };
+
+    struct IndexData
+    {
+        uint32_t* Indecies;
+        uint32_t Size;
+        SDL_GPUBuffer* IndexBuffer;
+        uint32_t BufferOffset;
+    };
+
+    std::vector<VertexData> Vertices;
+    std::vector<IndexData> Indecies;
+
+    SDL_GPUDevice* _graphicsDevice;
+};
